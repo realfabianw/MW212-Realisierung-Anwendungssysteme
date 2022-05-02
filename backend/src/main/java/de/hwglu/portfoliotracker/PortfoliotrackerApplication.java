50197469c868e16f1376bcefd32a61992890aa3f
Fabian <32239266+realfabianw@users.noreply.github.com>
2022-05-02 20:35:28 +0200

    add first call to Alphavantage

--- backend/src/main/java/de/hwglu/portfoliotracker/PortfoliotrackerApplication.java
+++ backend/src/main/java/de/hwglu/portfoliotracker/PortfoliotrackerApplication.java
@@ -1,5 +1,10 @@
 package de.hwglu.portfoliotracker;
 
+import com.crazzyghost.alphavantage.AlphaVantage;
+import com.crazzyghost.alphavantage.Config;
+import com.crazzyghost.alphavantage.timeseries.TimeSeries;
+import com.crazzyghost.alphavantage.timeseries.response.TimeSeriesResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -8,9 +13,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PortfoliotrackerApplication{
 	private static final Logger log = LoggerFactory.getLogger(PortfoliotrackerApplication.class);
+	private static final Config cfg = Config.builder().key("GCMWJ0M5WIZRV30O").timeOut(10).build();
 
 	public static void main(String[] args) {
-		SpringApplication.run(PortfoliotrackerApplication.class, args);		
+		AlphaVantage.api().init(cfg);
+
+		TimeSeriesResponse response = AlphaVantage.api().timeSeries().daily().forSymbol("IBM").fetchSync();
+
+		log.info(response.toString());
+
+		//SpringApplication.run(PortfoliotrackerApplication.class, args);
 	}
 
 }
