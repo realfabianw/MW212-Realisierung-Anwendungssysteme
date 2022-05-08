3d9a55eca09b94e6945c9214a8660b2d638d6146
Fabian <32239266+realfabianw@users.noreply.github.com>
2022-05-08 23:17:43 +0200

    code cleanup

---
+++ backend/src/main/java/de/hwglu/portfoliotracker/AlphaVantageConfig.java
@@ -0,0 +1,24 @@
+package de.hwglu.portfoliotracker;
+
+import com.crazzyghost.alphavantage.AlphaVantage;
+import com.crazzyghost.alphavantage.Config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+public class AlphaVantageConfig {
+
+    @Value("${alphavantage.key}")
+    private String key;
+
+    @Bean
+    public void initialise() {
+        AlphaVantage.api().init(Config.builder().key(key).timeOut(10).build());
+        log.info("AlphaVantage connection initialised with key: " + key);
+    }
+}
--- backend/src/main/java/de/hwglu/portfoliotracker/PortfoliotrackerApplication.java
+++ backend/src/main/java/de/hwglu/portfoliotracker/PortfoliotrackerApplication.java
@@ -1,17 +1,11 @@
 package de.hwglu.portfoliotracker;
 
-import com.crazzyghost.alphavantage.AlphaVantage;
-import com.crazzyghost.alphavantage.Config;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PortfoliotrackerApplication {
-	private static final Config cfg = Config.builder().key("GCMWJ0M5WIZRV30O").timeOut(10).build();
-
 	public static void main(String[] args) {
-		AlphaVantage.api().init(cfg);
 		SpringApplication.run(PortfoliotrackerApplication.class, args);
 	}
 }
