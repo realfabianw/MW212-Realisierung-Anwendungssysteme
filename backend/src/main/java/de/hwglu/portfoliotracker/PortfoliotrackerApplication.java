2af479c1e0e3c5bdabec99cc013d1aa83895fd33
Fabian <32239266+realfabianw@users.noreply.github.com>
2022-05-08 21:53:21 +0200

    code cleanup

--- backend/src/main/java/de/hwglu/portfoliotracker/PortfoliotrackerApplication.java
+++ backend/src/main/java/de/hwglu/portfoliotracker/PortfoliotrackerApplication.java
@@ -3,15 +3,11 @@ package de.hwglu.portfoliotracker;
 import com.crazzyghost.alphavantage.AlphaVantage;
 import com.crazzyghost.alphavantage.Config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PortfoliotrackerApplication {
-	private static final Logger log = LoggerFactory.getLogger(PortfoliotrackerApplication.class);
 	private static final Config cfg = Config.builder().key("GCMWJ0M5WIZRV30O").timeOut(10).build();
 
 	public static void main(String[] args) {
