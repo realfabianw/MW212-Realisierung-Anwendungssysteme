4b0c7a0322361f05515b2a4ad124f5f97e94c6ad
Fabian <32239266+realfabianw@users.noreply.github.com>
2022-05-09 17:01:55 +0200

    add cors

--- backend/src/main/java/de/hwglu/portfoliotracker/security/boundary/WebSecurityConfig.java
+++ backend/src/main/java/de/hwglu/portfoliotracker/security/boundary/WebSecurityConfig.java
@@ -1,5 +1,8 @@
 package de.hwglu.portfoliotracker.security.boundary;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +15,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import de.hwglu.portfoliotracker.security.control.JwtAuthenticationEntryPoint;
 import de.hwglu.portfoliotracker.security.control.JwtAuthenticationFilter;
@@ -41,24 +47,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http
-                .cors()
-                .and()
-                .csrf()
-                .disable()
-                .exceptionHandling()
-                .authenticationEntryPoint(unauthorizedHandler)
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests()
-                .antMatchers("/register", "/login")
-                .permitAll()
-                .anyRequest()
-                .authenticated();
+
+        // http.csrf();
+        // .disable()
+
+        http.cors();
+        http.csrf().disable();
+        http.exceptionHandling();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests()
+                .antMatchers("/register", "/login").permitAll()
+                .anyRequest().authenticated();
 
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
 }
